2cb98959708802de254f539a9c41b6a6df2a2eec
yeruza77 <vergeldiego26@gmail.com>
2024-09-15 09:53:56 -0400

    first commit

---
+++ react_desafio1/src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar"
+import Home from "./components/Home"
+import Footer from "./components/Footer"
+import './App.css';
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
